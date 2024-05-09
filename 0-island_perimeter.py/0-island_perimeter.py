9b97b5e1305e51138ddc6d912bd9a46b10343596
Keely001 <thindiuke@gmail.com>
2024-05-09 11:12:46 +0000

    Olly codes

--- 0-island_perimeter.py/0-island_perimeter.py
+++ 0-island_perimeter.py/0-island_perimeter.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Defines island perimeter finding function."""
+"""
+    function def island_perimeter(grid): that returns
+    the perimeter of the island described in grid:
+"""
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-    The grid represents water by 0 and land by 1.
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
+    """return the perimeter of the island described in grid"""
+    perimeter = 0
+    i = 0
+    while i < len(grid):
+        j = 0
+        while j < len(grid[i]):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                perimeter += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+            j += 1
+        i += 1
+    return perimeter
